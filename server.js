3fd45c4e3455e8dcc32f9caa58823a4cceb471ca
Hisataka Ishii <scraper.once@gmail.com>
2010-12-07 23:04:11 +0900

    little fix

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
  * MeSH starter
  */
 var app = require("./init");
- 
+
 MESH_PORT = 3000;
  
 // Only listen on $ node app.js
 
 if (!module.parent) {
 	app.listen(MESH_PORT);
-	console.log("Express server listening on port %d", app.address().port)
+	console.log("Express server listening on port %d", app.address().port);
 }
